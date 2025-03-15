303adf7bbd43630cddb0eba114b7ddff3c41e7db
Ryan <103166889+Ryan-Guizhou@users.noreply.github.com>
2025-03-16 01:12:15 +0800

    完善swagger注解

--- src/main/java/com/peach/common/entity/qo/LogQO.java
+++ src/main/java/com/peach/common/entity/qo/LogQO.java
@@ -21,24 +21,24 @@ public class LogQO extends BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(name = "操作等级")
+    @ApiModelProperty(value = "操作等级",required = true)
     private String optLevel;
 
-    @ApiModelProperty(name = "操作类型编码")
+    @ApiModelProperty(value = "操作类型编码")
     private String optTypeCode;
 
-    @ApiModelProperty(name = "模块编码")
+    @ApiModelProperty(value = "模块编码")
     private String moduleCode;
 
-    @ApiModelProperty(name = "创建人名称")
+    @ApiModelProperty(value = "创建人名称")
     private String creatorName;
 
-    @ApiModelProperty(name = "开始时间")
+    @ApiModelProperty(value = "开始时间")
     private String startTime;
 
-    @ApiModelProperty(name = "结束时间")
+    @ApiModelProperty(value = "结束时间")
     private String endTime;
 
-    @ApiModelProperty(name = "请求是否成功")
+    @ApiModelProperty(value = "请求是否成功")
     private String isSuccess;
 }
